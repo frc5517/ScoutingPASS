e5c24458bceb934c875b684381ca9b1821f7dbee
KonnorR <konnor0601@gmail.com>
2024-02-29 14:01:14 -0500

    TBA Auth
    Updated The Blue Alliance API authKey to our own key.

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "UfZA6Bl277DABBzT5vAfY1t8bxPgGrpweZB8GQxkRZ8JBPoygfecVXXOnsUHHRpb";
 /**
  * Get list of teams in event
  *
